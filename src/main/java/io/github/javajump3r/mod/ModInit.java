96a2b004f55d6c52c5a74bff64de7eb0603171fb
JavaJump3r <59884606+JavaJump3r@users.noreply.github.com>
2023-05-08 14:29:16 +0300

    Initial commit

---
+++ src/main/java/io/github/javajump3r/mod/ModInit.java
@@ -0,0 +1,10 @@
+package io.github.javajump3r.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModInit implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
